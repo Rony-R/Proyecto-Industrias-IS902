c58692c50c11a127d28385403045624e2037b2d5
JB_Royale <jheral.blanco@gmail.com>
2021-03-16 01:11:57 -0600

    test(perfil-info-personal): Se realiza prueba de conexion con la base de datos

--- ajax/perfil.php
+++ ajax/perfil.php
@@ -1,19 +1,43 @@
 <?php
-// include_once('../class/class-conexion.php');
-// $conexion = new Conexion();
+include_once('../class/class-conexion.php');
+$conexion = new Conexion();
 
 switch ($_GET["accion"]) {
   case "1":
+    $query = 'select * from tbl_tecnologias';
+    $resQuery = $conexion->ejecutarConsulta($query);
+
     $json = array();
-    $json[] = array(
-      'nombre' => 'Jheral Blanco'
-    );
-    $jsonHola = json_encode($json);
-    echo $jsonHola;
+
+    while ($fila = $conexion->obtenerFila($resQuery)) {
+      $json[] = array(
+        'id' => $fila['id_tecnologia'],
+        'tecnologia' => $fila['tecnologia']
+      );
+    }
+    
+    $jsonString = json_encode($json);
+    echo $jsonString;
+    break;
+  case "2":
+    $query = 'select * from tbl_paises';
+    $resQuery = $conexion->ejecutarConsulta($query);
+
+    $json = array();
+
+    while ($fila = $conexion->obtenerFila($resQuery)) {
+      $json[] = array(
+        'id' => $fila['id_pais'],
+        'pais' => $fila['pais']
+      );
+    }
+    
+    $jsonString = json_encode($json);
+    echo $jsonString;
     break;
   
   default:
-    echo ("No da ni madres");
+    echo ("nulllllllll");
     break;
 }
 
--- js/controlador-perfil.js
+++ js/controlador-perfil.js
@@ -1,11 +1,12 @@
-let  obtenerTecnologias = () =>{
+const  obtenerTecnologias = () =>{
   $.ajax({
-    url: "ajax/perfil.php?accion=1",
+    url: "ajax/perfil.php?accion=1", // * Obtener las tecnologias
     type: "GET",
     dataType: 'json',
     success:function(response){
-      console.log(response);
-      
+      response.forEach(tecnologia => {
+        console.log(`id: ${tecnologia.id}, Tecnologia: ${tecnologia.tecnologia}`);
+      });
     },
     error:function(e){
         console.log(e);
@@ -13,3 +14,20 @@ let  obtenerTecnologias = () =>{
   });
 }
 obtenerTecnologias();
+
+const obtenerPaises = () => {
+  $.ajax({
+    url: "ajax/perfil.php?accion=2", // * Obtener los paises
+    type: "GET",
+    dataType: 'json',
+    success:function(response){
+      response.forEach(pais => {
+        console.log(`Id: ${pais.id}, Pais: ${pais.pais}`);
+      });
+    },
+    error:function(e){
+        console.log(e);
+    }
+  });
+}
+obtenerPaises();
