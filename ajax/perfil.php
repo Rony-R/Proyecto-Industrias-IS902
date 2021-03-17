d76faf5fbd608b16919e327992e4a4b3c14b8a63
JB_Royale <jheral.blanco@gmail.com>
2021-03-16 22:45:26 -0600

    [FEAT](Perfil-info-personal): mostrar info personal freelancer

--- ajax/perfil.php
+++ ajax/perfil.php
@@ -35,6 +35,52 @@ switch ($_GET["accion"]) {
     $jsonString = json_encode($json);
     echo $jsonString;
     break;
+  case "3":
+    $idUsuario = $_GET['idUsuario'];
+
+    // Obteniendo datos del usuario
+    $query = "select * from tbl_usuario u
+              inner join tbl_paises p on p.id_pais = u.id_pais
+              inner join tbl_tipo_usuario tp on tp.id_tipo_usuario = u.id_tipo_usuario
+              where u.id_usuario = $idUsuario";
+    $resQuery = $conexion->ejecutarConsulta($query);
+
+    // Obteniendo tecnologias del usuario
+    $query2 = "select t.tecnologia tec from tbl_usuario u
+              inner join tbl_tec_x_usuario tu on tu.id_usuario = u.id_usuario
+              inner join tbl_tecnologias t on t.id_tecnologia = tu.id_tecnologia 
+              where u.id_usuario = $idUsuario";    
+    $resQuery2 = $conexion->ejecutarConsulta($query2);
+
+    // Obteniendo los proyectos en los que ha trabajado el usuario con devFinder
+    
+
+    $json[] = array();
+
+    while ($fila = $conexion->obtenerFila($resQuery2)) {
+      $json[0]['tecnologias'][] = array(
+        $fila['tec']
+      );
+    }
+
+    $fila = $conexion->obtenerFila($resQuery);
+    $json[0]['info'][] = array(
+      'idUsuario' => $fila['id_usuario'],
+      'tipoUsuario' => $fila['tipo_usuario'],
+      'pais' => $fila['pais'],
+      'nombre' => $fila['nombre'],
+      'apellido' => $fila['apellido'],
+      'direccion' => $fila['direccion'],
+      'correo' => $fila['correo'],
+      'telefono' => $fila['telefono'],
+      'contrasenia' => $fila['contrasenia'],
+      'rutaImgPerfil' => $fila['ruta_img_perfil'],
+      'nombreImgPerfil' => $fila['nombre_img_perfil']
+    );
+
+    $jsonString = json_encode($json);
+    echo $jsonString;
+    break;
   
   default:
     echo ("nulllllllll");
--- js/controlador-perfil.js
+++ js/controlador-perfil.js
@@ -1,4 +1,58 @@
-const  obtenerTecnologias = () =>{
+let tipoUsuario = 1;
+let idUsuario = 2;
+let parametros = `idUsuario=${idUsuario}`
+
+if (tipoUsuario == 1) {
+  
+  console.log('Es freelancer');
+  document.getElementById("navPagos").style.display = "none";
+  document.getElementById("navPublicaciones").style.display = "none";
+
+
+  } else if (tipoUsuario == 2){
+  console.log('Es una empresa');
+  document.getElementById("cardExperienciaLaboral").style.display = "none";
+  document.getElementById("divApellido").style.display = "none";
+} else {
+  console.log('Debe loguearse para utilizar estas funciones');
+}
+
+const llenarInfo = () => {
+  $.ajax({
+    url: "ajax/perfil.php?accion=3", // * Obtener usuario
+    type: "GET",
+    data: parametros,
+    dataType: 'json',
+    success: function (response) {
+      console.log(response);
+      document.getElementById('navNombre').innerHTML = `${response[0].info[0].nombre} ${response[0].info[0].apellido}`;
+      document.getElementById('navImgPerfil').src = `./img/${response[0].info[0].rutaImgPerfil}/${response[0].info[0].nombreImgPerfil}`;
+
+      document.getElementById('nombre').innerHTML = `${response[0].info[0].nombre}`;
+      document.getElementById('apellido').innerHTML = `${response[0].info[0].apellido}`;
+      document.getElementById('direccion').innerHTML = `${response[0].info[0].direccion}`;
+      document.getElementById('correo').innerHTML = `${response[0].info[0].correo}`;
+      document.getElementById('telefono').innerHTML = `${response[0].info[0].telefono}`;
+      document.getElementById('contrasenia').innerHTML = `${response[0].info[0].contrasenia}`;
+      document.getElementById('imagenPerfil').src = `./img/${response[0].info[0].rutaImgPerfil}/${response[0].info[0].nombreImgPerfil}`;
+      document.getElementById('pais').innerHTML = `${response[0].info[0].pais}`;
+
+      let cadenaTecnologia = '';
+      let hTecnologias = document.getElementById('hTec');
+      hTecnologias.innerHTML = ``;
+      response[0].tecnologias.forEach(tec => {
+        cadenaTecnologia += `${tec}, `;
+      });
+      hTecnologias.innerHTML = `${cadenaTecnologia.slice(0,-2)}`;
+    },
+    error: function (e) {
+      console.log(e);
+    }
+  });
+}
+llenarInfo();
+
+const obtenerTecnologias = () => {
   $.ajax({
     url: "ajax/perfil.php?accion=1", // * Obtener las tecnologias
     type: "GET",
