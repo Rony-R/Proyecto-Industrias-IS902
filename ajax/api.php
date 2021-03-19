2e6cf115a56e12ec7dc2f1a8d813b69dcff4a555
Rony-R <ronyrodriguez2011@hotmail.com>
2021-03-19 12:40:12 -0600

    Estructura backend consultas datos pub proyecto

--- ajax/api.php
+++ ajax/api.php
@@ -1,23 +1,36 @@
 <?php
     include("../class/class-conexion.php");
 
+    $conexion = new Conexion();
+
     switch($_GET["accion"]){
-<<<<<<< HEAD
-        case "obtener-comentario-publicacion":
-=======
-        case "'publicarProyecto'":
-            echo ("Hola php");
->>>>>>> b05351c419edad4074f920b55433aad493715aa9
+
+        case "'traerPresupuestos'":
+            $sql = "SELECT * FROM TBL_PRESUPUESTO;";
+            $resultado = $conexion->ejecutarConsulta($sql);
+            $rJson = json_encode($resultado);
+            echo $rJson;
         break;
 
-        case "agregar-comentario-publicacion":
+        case "'traerTipoProyectos'":
+            $sql = "SELECT * FROM TBL_CATEGORIA_PROYECTO;";
+            $resultado = $conexion->ejecutarConsulta($sql);
+            $rJson = json_encode($resultado);
+            echo $rJson;
         break;
 
-        case "accion":
+        case "'accion'":
         break;
 
-        case "accion":
+        case "'accion'":
         break;
+
+        case "'accion'":
+        break;
+
+        case "'accion'":
+        break;
+
     }
 
 ?>
