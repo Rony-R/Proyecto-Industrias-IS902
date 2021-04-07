e0bc149eb07b7acd9d1dbcc8dafe60cc526861ce
LPSoldier19 <rafael.bautista1@hotmail.es>
2021-04-07 17:54:05 -0600

    Probando posible error

--- info-publicacion.php
+++ info-publicacion.php
@@ -1,10 +1,7 @@
-<?php
-
-  $publicacion = $_GET["publicacion"];
-
-?>
-
 <?php 
+
+    $publicacion = $_GET["publicacion"];
+
     session_start();
 
     if (!isset($_SESSION["usr"]) || !isset($_SESSION["psw"]))
